4e6f8afa1dd15f6f73920ae288d9909b72eaeda5
luoxi <2652445066@qq.com>
2021-09-07 00:57:29 +0800

    卡片展开

---
+++ 卡片展开/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-06 23:03:08
+ * @LastEditTime: 2021-09-07 00:22:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \css3-exercise\卡片展开\index.js
+ */
+
+var card = document.querySelector('.card')
+card.addEventListener('click', function() {
+    if (card.className == 'card') {
+        card.className = 'card active'
+    } else {
+        card.className = 'card'
+    }
+
+}, false);
